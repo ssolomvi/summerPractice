dc83e1199601607ce0421dfc75ee685e2f7da358
ssolomvi <ssolomvi@gmail.com>
2023-07-07 10:28:11 +0300

    untied derived classes in one

--- summer_practice_domain/SortingMethods.cs
+++ summer_practice_domain/SortingMethods.cs
@@ -66,8 +66,81 @@ public interface ISortingMethods
     public T[] Sort<T>(T[] toSort, SortingMode sortingMode, SortingMethod sortingMethod, Comparison<T> comparison);
 }
 
-public class InsertionSort : ISortingMethods
+public class SortingMethodsImpl : ISortingMethods
 {
+    private static void Swap<T>(Span<T> a, int i, int j)
+    {
+        // Debug.Assert(i != j);
+
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static T[] HeapSort<T>(T[] keys, Comparison<T> comparer)
+    {
+        // Debug.Assert(comparer != null);
+        // Debug.Assert(!keys.IsEmpty);
+
+        int n = keys.Length;
+        for (int i = n >> 1; i >= 1; i--)
+        {
+            DownHeap(keys, i, n, comparer);
+        }
+
+        for (int i = n; i > 1; i--)
+        {
+            Swap(keys, 0, i - 1);
+            DownHeap(keys, 1, i - 1, comparer);
+        }
+    }
+
+    private static void DownHeap<T>(Span<T> keys, int i, int n, Comparison<T> comparer)
+    {
+        // Debug.Assert(comparer != null);
+
+        T d = keys[i - 1];
+        while (i <= n >> 1)
+        {
+            int child = 2 * i;
+            if (child < n && comparer(keys[child - 1], keys[child]) < 0)
+            {
+                child++;
+            }
+
+            if (!(comparer(d, keys[child - 1]) < 0))
+                break;
+
+            keys[i - 1] = keys[child - 1];
+            i = child;
+        }
+
+        keys[i - 1] = d;
+    }
+
+    private static T[] InsertionSorting<T>(T[] keys, ISortingMethods.SortingMode sortingMode, IComparer<T> comparer)
+    {
+        T[] toReturn = new T[keys.Length];
+        toReturn[0] = keys[0];
+        
+        for (int i = 0; i < keys.Length - 1; i++)
+        {
+            T t = keys[i + 1];
+
+            int j = i;
+            while (j >= 0 && (sortingMode == ISortingMethods.SortingMode.Ascending ^
+                              comparer.Compare(t, toReturn[j]) < 0))
+            {
+                toReturn[j + 1] = toReturn[j];
+                j--;
+            }
+                
+            if ((j + 1) != i) toReturn[j + 1] = t;
+        }
+
+        return toReturn;
+    }
+
+    /*
     private static void InsertionSorting<T>(T[] keys, IComparer<T> comparer)
     {
         for (int i = 0; i < keys.Length - 1; i++)
@@ -81,24 +154,39 @@ public class InsertionSort : ISortingMethods
                 j--;
             }
                 
-            if (j != i) keys[j + 1] = t;
+            keys[j + 1] = t;
         }
     }
-
+*/
     public T[] Sort<T>(T[] toSort, ISortingMethods.SortingMode sortingMode, ISortingMethods.SortingMethod sortingMethod)
         where T : System.IComparable<T>
     {
-        throw new NotImplementedException();
+        var comparer = Comparer<T>.Default;
+        return Sort(toSort, sortingMode, sortingMethod, comparer);
+        // return Sort(toSort, sortingMode, sortingMethod, comparer.Compare);
     }
 
     public T[] Sort<T>(T[] toSort, ISortingMethods.SortingMode sortingMode, ISortingMethods.SortingMethod sortingMethod, IComparer<T> comparer)
     {
-        throw new NotImplementedException();
+        switch (sortingMethod)
+        {
+            case ISortingMethods.SortingMethod.InsertionSort:
+                return InsertionSorting(toSort, sortingMode, comparer);
+            case ISortingMethods.SortingMethod.SelectionSort:
+                return;
+            case ISortingMethods.SortingMethod.MergeSort:
+                return;
+            case ISortingMethods.SortingMethod.HeapSort:
+                return;
+            default: ISortingMethods.SortingMethod.QuickSort:
+                return;
+        }
     }
 
     public T[] Sort<T>(T[] toSort, ISortingMethods.SortingMode sortingMode, ISortingMethods.SortingMethod sortingMethod, Comparison<T> comparison)
     {
-        throw new NotImplementedException();
+        var comparer = Comparer<T>.Create(comparison);
+        return Sort(toSort, sortingMode, sortingMethod, comparer);
     }
 }
 
@@ -123,49 +211,6 @@ public class SelectionSort : ISortingMethods
 
 public class HeapSort : ISortingMethods
 {
-    /*
-    private static void HeapSort<T>(Span<T> keys, Comparison<T> comparer)
-    {
-        Debug.Assert(comparer != null);
-        Debug.Assert(!keys.IsEmpty);
-
-        int n = keys.Length;
-        for (int i = n >> 1; i >= 1; i--)
-        {
-            DownHeap(keys, i, n, comparer);
-        }
-
-        for (int i = n; i > 1; i--)
-        {
-            Swap(keys, 0, i - 1);
-            DownHeap(keys, 1, i - 1, comparer);
-        }
-    }
-*/
-    private static void DownHeap<T>(Span<T> keys, int i, int n, Comparison<T> comparer)
-    {
-        // Debug.Assert(comparer != null);
-
-        T d = keys[i - 1];
-        while (i <= n >> 1)
-        {
-            int child = 2 * i;
-            if (child < n && comparer(keys[child - 1], keys[child]) < 0)
-            {
-                child++;
-            }
-
-            if (!(comparer(d, keys[child - 1]) < 0))
-                break;
-
-            keys[i - 1] = keys[child - 1];
-            i = child;
-        }
-
-        keys[i - 1] = d;
-    }
-
-    
     public T[] Sort<T>(T[] toSort, ISortingMethods.SortingMode sortingMode, ISortingMethods.SortingMethod sortingMethod)
         where T : System.IComparable<T>
     {
