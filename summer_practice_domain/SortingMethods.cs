b19704dfddf971f8cf8db975336c2565b36d9467
ssolomvi <ssolomvi@gmail.com>
2023-07-08 00:39:51 +0300

    minor fixes

--- summer_practice_domain/SortingMethods.cs
+++ summer_practice_domain/SortingMethods.cs
@@ -1,47 +1,5 @@
 namespace summer_practice_domain;
 
-/*
- * Реализуйте набор перегруженных методов расширения для сортировки обобщённых массивов данных. В качестве первого
- * параметра методы получают на вход коллекцию сортируемых данных типа T[]. В качестве второго - порядок сортировки
- * (по возрастанию, по убыванию) - в виде значения собственного перечислимого типа (enum). Третий параметр - алгоритм
- * сортировки (вставками, выбором, пирамидальная, быстрая, слиянием).
- * Методы перегружены засчёт четвёртого параметра:
- * ● его нет (должен быть обеспечено ограничение внутренней сравнимости на предмет отношения порядка элементов массива);
- * ● значение типа IComparer<T>, задающее внешнее правило отношения порядка на пространстве элементов типа T;
- * ● значение типа Comparer<T>, задающее внешнее правило отношения порядка на пространстве элементов типа T;
- * ● делегат Comparison<T>, на объект которого подписан метод, задающий внешнее отношение порядка на пространстве
- *   элементов типа T.
- * Возвращаемое значение метода типа T[] - отсортированный массив.
- * 
- */
-/*
-internal static int InternalBinarySearch(T[] array, int index, int length, T value, IComparer<T> comparer)
-{
-    // todo Assert(array != null, "Check the arguments in the caller!");
-    // todo Assert(index >= 0 && length >= 0 && (array.Length - index >= length), "Check the arguments in the caller!");
-
-    int lo = index;
-    int hi = index + length - 1;
-    while (lo <= hi)
-    {
-        int i = lo + ((hi - lo) >> 1);
-        int order = comparer.Compare(array[i], value);
-
-        if (order == 0) return i;
-        if (order < 0)
-        {
-            lo = i + 1;
-        }
-        else
-        {
-            hi = i - 1;
-        }
-    }
-
-    return ~lo;
-}
-*/
-
 public static class SortingMethodsImpl
 {
     public enum SortingMode
